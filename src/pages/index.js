7abf11784eff4f26827af454280a0abae74114cf
halfrice <halfrice@pm.me>
2020-07-09 00:10:59 -0700

    Add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <h1>Racer Inc</h1>
+  return (
+    <Layout>
+      <h1>Racer Inc</h1>
+    </Layout>
+  )
 }
